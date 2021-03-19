d7afedded5da0a118d0642a621dc5322bdae3acf
ugurkarakurt <ugurkarakurt57@gmail.com>
2021-03-19 20:32:32 +0300

    -Fix bugs

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
 //use the import method to use functions and webpack.
 import UI from "./ui";
-import {
-    products
-} from "./request";
+import Request from "./request";
 
-console.log(products);
+const productsDataURL = "../productsAPI/products.json"
 
 //send a get request with the request method from the request file.
 //created a sync structure with .then.
-
-const getProducts = () => {
-    console.log(products);
+Request.get(productsDataURL).then(data => {
+    console.log(data.products);
     //send the incoming data as a parameter to the ui function to use.
-    UI.getProductsToUI(products)
-}
-getProducts()
+    UI.getProductsToUI(data.products)
+}).then(() => {
+    //use queryselectorAll method to use slider library everywhere at once.
+    const productWrappers = document.querySelectorAll(".product-wrapper")
 
-//use queryselectorAll method to use slider library everywhere at once.
-const productWrappers = document.querySelectorAll(".product-wrapper")
-
-productWrappers.forEach(productWrapper => {
-    $(productWrapper).slick({
-        infinite: true,
-        speed: 100,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        dots: false,
-        nextArrow: '<a style="font-size: 1rem;\
+    productWrappers.forEach(productWrapper => {
+        $(productWrapper).slick({
+            infinite: true,
+            speed: 100,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: false,
+            nextArrow: '<a style="font-size: 1rem;\
             position: absolute;\
             top: 50%;\
             right: -.6rem;\
@@ -34,7 +29,7 @@ productWrappers.forEach(productWrapper => {
             color: #ffffff;\
             "\
             type="button" class="fas fa-chevron-right"></a>',
-        prevArrow: '<a style="font-size: 1rem;\
+            prevArrow: '<a style="font-size: 1rem;\
             position: absolute;\
             top: 50%;\
             left: -.6rem;\
@@ -42,28 +37,29 @@ productWrappers.forEach(productWrapper => {
             color: #ffffff;\
             "\
             type="button" class="fas fa-chevron-left"></a>',
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
                 }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+            ]
+        });
     });
-});
+})
